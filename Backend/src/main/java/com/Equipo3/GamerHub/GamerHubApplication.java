ca5d8ff6d870ca24301fc0352415e0fed05ce13f
Manuel Santos <48207433+Bluishres@users.noreply.github.com>
2021-02-03 11:14:05 +0100

    Backend configurado

---
+++ Backend/src/main/java/com/Equipo3/GamerHub/GamerHubApplication.java
@@ -0,0 +1,13 @@
+package com.Equipo3.GamerHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamerHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamerHubApplication.class, args);
+	}
+
+}

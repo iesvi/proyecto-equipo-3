e230d2982801c60a69d90d458d3431723cc6baa7
Manuel Santos <48207433+Bluishres@users.noreply.github.com>
2021-02-16 09:49:16 +0100

    backend estructurado

--- Backend/src/test/java/com/Equipo3/Backend/Evento/Dominio/Repository/EventoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Equipo3.Backend.Evento.Dominio.Repository;
-
-import com.Equipo3.Backend.Evento.Dominio.Evento;
-import com.Equipo3.Backend.Shared.Dominio.Repository.GenericRepository;
-
-public interface EventoRepository extends GenericRepository<Evento,Integer> {
-
-}
--- Backend/src/test/java/com/Equipo3/Backend/Usuario/Dominio/Repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Equipo3.Backend.Usuario.Dominio.Repository;
-
-import com.Equipo3.Backend.Shared.Dominio.Repository.GenericRepository;
-import com.Equipo3.Backend.Usuario.Dominio.Usuario;
-
-public interface UsuarioRepository extends GenericRepository<Usuario,Integer> {
-
-}

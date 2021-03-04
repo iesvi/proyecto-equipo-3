556af21d011a8a6a5b127e5c9810f97310fe38e0
jssdocente <jssgarcia.docente@gmail.com>
2021-03-04 10:11:31 +0100

    [PROFE|FIX] RUN-APP REMOVE SOCKET REFERENCE

--- Backend/src/main/java/com/Equipo3/Backend/app/RunApp.java
+++ Backend/src/main/java/com/Equipo3/Backend/app/RunApp.java
@@ -1,17 +1,11 @@
 package com.Equipo3.Backend.app;
 
-import com.Equipo3.Backend.Shared.Infraestructura.Socket.SocketServerHandler;
-import com.Equipo3.Backend.Usuario.Dominio.DTO.UsuarioDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RunApp {
 
-    @Autowired
-    SocketServerHandler handler;
-
     public void run() {
-        handler.send(new UsuarioDTO());
+
     }
 }
